ecee96efd86e51c74976463a8165ea06b7c1d02d
unknown <peterlim995@gmail.com>
2023-01-07 01:53:57 -0500

    clean the code by make every function async/await

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const inquirer = require('inquirer');
-const mysql = require('mysql2');
 const logo = require('asciiart-logo');
 const config = require('./package.json');
 const cTable = require('console.table');
-const e = require('express');
 const db = require('./db/connection');
-const EmployeeTracker = require('./db')
+const EmployeeTracker = require('./db');
+// const questions = require('./db/prompt');
 
 
 console.log(logo(config).render());
@@ -13,63 +12,73 @@ console.log(logo(config).render());
 const employeeTracker = new EmployeeTracker(db);
 
 // Prompt the menu
-function menu() {
-    inquirer
-        .prompt([
-            {
-                type: 'list',
-                name: 'todo',
-                message: 'What would you like to do?',
-                choices: ['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Role', 'Add Role', 'View All Departments', 'Add Department', 'Quit'],
-            },
-        ])
-        .then(answer => {
-            const { todo } = answer;
-            switch (todo) {
-                case 'View All Employees':
-                    viewAllEmployees();
-                    break;
-                case 'Add Employee':
-                    addEmployee();
-                    break;
-                case 'Update Employee Role':
-                    updateEmployeeRole();
-                    break;
-                case 'View All Role':
-                    viewAllRole();
-                    break;
-                case 'Add Role':
-                    addRole();
-                    break;
-                case 'View All Departments':
-                    viewAllDepartments();
-                    break;
-                case 'Add Department':
-                    addDepartment();
-                    break;
-                case 'Quit':
-                    quit();
-                    break;
-            }
-        })
+async function menu() {
+
+    try {
+        const answer = await inquirer
+            .prompt([
+                {
+                    type: 'list',
+                    name: 'todo',
+                    message: 'What would you like to do?',
+                    choices: ['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Role', 'Add Role', 'View All Departments', 'Add Department', 'Quit'],
+                },
+            ]);
+
+        const { todo } = answer;
+        switch (todo) {
+            case 'View All Employees':
+                viewAllEmployees();
+                break;
+            case 'Add Employee':
+                addEmployee();
+                break;
+            case 'Update Employee Role':
+                updateEmployeeRole();
+                break;
+            case 'View All Role':
+                viewAllRole();
+                break;
+            case 'Add Role':
+                addRole();
+                break;
+            case 'View All Departments':
+                viewAllDepartments();
+                break;
+            case 'Add Department':
+                addDepartment();
+                break;
+            case 'Quit':
+                quit();
+                break;
+        }
+    }
+    catch (err) {
+        console.error(err)
+    }
 }
 
-function viewAllEmployees() {
 
-    employeeTracker.viewEmployee()
-        .then(async results => {
-            for (let i = 0; i < results.length; i++) {
-                const result = results[i];
-                if (result.manager) {
-                    result.manager = await employeeTracker.getManagerId(result.manager);
-                }
+
+// Show all Employees
+async function viewAllEmployees() {
+
+    try {
+        const results = await employeeTracker.viewEmployee();
+
+        for (let i = 0; i < results.length; i++) {
+            const result = results[i];
+            if (result.manager) {
+                result.manager = await employeeTracker.getManagerId(result.manager);
             }
-            console.table('\n', results);
-            menu();
-        })
-        .catch(err =>
-            console.error(err)
-        );
+        }
+        console.table('\n', results);
+        menu();
+    }
+    catch (err) {
+        console.error(err)
+    }
+
 }
 
 // Role title list return
@@ -90,24 +99,26 @@ async function managerList() {
 
     // remove duplicate id
     employee.filter(result => result.manager != null).forEach(result => {
-        if(employeeIdList.indexOf(result.manager) === -1)
+        if (employeeIdList.indexOf(result.manager) === -1)
             employeeIdList.push(result.manager);
     });
 
     // create Manager full name list
-    for (let i = 0; i < employeeIdList.length; i++) {        
-        employeeList.push(await employeeTracker.getManagerId(employeeIdList[i]));        
+    for (let i = 0; i < employeeIdList.length; i++) {
+        employeeList.push(await employeeTracker.getManagerId(employeeIdList[i]));
     }
     return employeeList;
 }
 
+
+// Add employee
 async function addEmployee() {
 
-    const titles = await roleList();
-    const managerNames = await managerList();
+    try {
+        const titles = await roleList();
+        const managerNames = await managerList();
 
-    inquirer
-        .prompt([
+        const answer = await inquirer.prompt([
             {
                 type: 'input',
                 name: 'firstname',
@@ -130,46 +141,47 @@ async function addEmployee() {
                 message: `Who is the employee's manager?`,
                 choices: ['None', ...managerNames],
             },
-        ])
-        .then(async answer => {
-            const { firstname, lastname, role, manager } = answer;
+        ]);
 
-            try {
-                const roleId = await employeeTracker.roleIdbyTitle(role);
-                let results;
+        const { firstname, lastname, role, manager } = answer;
 
-                // No manager
-                if(manager === 'None'){
-                    results = await employeeTracker.createEmployee([firstname, lastname, roleId]);
-                } else {
-                    const managerId = await employeeTracker.employeeIdByFullName(manager);
-                    results = await employeeTracker.createEmployee([firstname, lastname, roleId, managerId]);
-                }
-                
-                console.log('\n', results);
-                menu();
 
-            } catch (err) {
-                console.error(err)
-            }            
-        })
+        const roleId = await employeeTracker.roleIdbyTitle(role);
+        let results;
+
+        // if No manager
+        if (manager === 'None') {
+            results = await employeeTracker.createEmployee([firstname, lastname, roleId]);
+        } else {
+            const managerId = await employeeTracker.employeeIdByFullName(manager);
+            results = await employeeTracker.createEmployee([firstname, lastname, roleId, managerId]);
+        }
+
+        console.log('\n', results);
+        menu();
+
+    } catch (err) {
+        console.error(err)
+    }
+
 }
 
 // return employee's full name list array
 async function employeeList() {
     const employees = await employeeTracker.viewEmployee();
-    return employees.map(result => result.first_name+' '+result.last_name);
+    return employees.map(result => result.first_name + ' ' + result.last_name);
 }
 
+// Update Employee Role
 async function updateEmployeeRole() {
 
-    // employee's full name array
-    const employees = await employeeList();
-    // role title list array
-    const titles = await roleList();
+    try {
+        // employee's full name array
+        const employees = await employeeList();
+        // role title list array
+        const titles = await roleList();
 
-    inquirer
-        .prompt([
+        const answer = await inquirer.prompt([
             {
                 type: 'list',
                 name: 'employeeName',
@@ -182,51 +194,49 @@ async function updateEmployeeRole() {
                 message: `Which role do you want to assign the selected employee?`,
                 choices: titles,
             },
-        ])
-        .then(async answer => {
-            const { employeeName, role } = answer;
+        ]);
 
-            try {
-                const name = employeeName.split(' ');
-                const first_name = name[0];
-                const last_name = name[1];
+        const { employeeName, role } = answer;
+        const name = employeeName.split(' ');
+        const first_name = name[0];
+        const last_name = name[1];
 
-                const roleId = await employeeTracker.roleIdbyTitle(role);
-                const results = await employeeTracker.updateEmployee([roleId, first_name, last_name])
-                console.log('\n', results);
-                menu();
+        const roleId = await employeeTracker.roleIdbyTitle(role);
+        const results = await employeeTracker.updateEmployee([roleId, first_name, last_name])
+        console.log('\n', results);
+        menu();
 
-            } catch (err) {
-                console.error(err)
-            }            
-        })
+    } catch (err) {
+        console.error(err)
+    }
+
 }
 
-function viewAllRole() {
+// View all role
+async function viewAllRole() {
 
-    employeeTracker.viewRole()
-        .then(results => {
-            console.table('\n', results);
-            menu();
-        })
-        .catch(err =>
-            console.error(err)
-        );
+    try {
+        const results = await employeeTracker.viewRole();
+        console.table('\n', results);
+        menu();
+    }
+    catch (err) {
+        console.error(err)
+    }
 }
 
 // return array of department name
 async function departmentList() {
     const department = await employeeTracker.viewDepartment()
     return department.map(result => result.name);
-
 }
 
 // Add Role
 async function addRole() {
 
-    let departList = await departmentList();
-    inquirer
-        .prompt([
+    try {
+        const departList = await departmentList();
+        const answer = await inquirer.prompt([
             {
                 type: 'input',
                 name: 'role',
@@ -236,6 +246,11 @@ async function addRole() {
                 type: 'input',
                 name: 'salary',
                 message: 'What is the salary of the role?',
+                validate(value) {
+                    if (typeof parseInt(value) === 'number' && value >= 0)
+                        return true;
+                    return 'Salary must be number and greater than zero';
+                }
             },
             {
                 type: 'list',
@@ -243,58 +258,55 @@ async function addRole() {
                 message: 'Which department does the role belong to?',
                 choices: departList,
             },
-        ])
-        .then(async answer => {
-            const { role, salary, department } = answer;
+        ]);
 
-            try {
-                const departmentId = await employeeTracker.departmentIdbyName(department);
+        const { role, salary, department } = answer;
+        const departmentId = await employeeTracker.departmentIdbyName(department);
+        const results = await employeeTracker.createRole([role, salary, departmentId])
+        console.log('\n', results);
+        menu();
 
-                const results = await employeeTracker.createRole([role, salary, departmentId])
+    } catch (err) {
+        console.error(err)
+    }
 
-                console.log('\n', results);
-                menu();
-
-            } catch (err) {
-                console.error(err)
-            }
-        })
 }
 
 // View all Department
-function viewAllDepartments() {
+async function viewAllDepartments() {
 
-    employeeTracker.viewDepartment()
-        .then(results => {
-            console.table('\n', results);
-            menu();
-        })
-        .catch(err =>
-            console.error(err)
-        );
+    try {
+        const results = await employeeTracker.viewDepartment();
+        console.table('\n', results);
+        menu();
+    }
+    catch (err) {
+        console.error(err);
+    }
 }
 
 // Add Department
-function addDepartment() {
-    inquirer
-        .prompt([
+async function addDepartment() {
+
+    try {
+        const answer = await inquirer.prompt([
             {
                 type: 'input',
                 name: 'department',
                 message: 'What is the name of the department?',
             },
-        ])
-        .then(answer => {
-            const { department } = answer;
-            employeeTracker.createDepartment(department)
-                .then(results => {
-                    console.log('\n', results);
-                    menu();
-                })
-                .catch(err =>
-                    console.error(err)
-                );
-        })
+        ]);
+
+        const { department } = answer;
+        const results = await employeeTracker.createDepartment(department);
+
+        console.log('\n', results);
+        menu();
+    }
+    catch (err) {
+        console.error(err)
+
+    }
 
 }
 
@@ -304,10 +316,5 @@ function quit() {
     process.exit(0);
 }
 
-function test() {
-    return new Promise((rs, rj) => {
-        rs('Peter Lim');
-    });
-}
 
 menu();
